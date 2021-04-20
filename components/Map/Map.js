58e285732f30d9680a45ff70c8f6968390e9770c
devgeetech <devgeetech@gmail.com>
2021-04-20 09:39:08 +0530

    location change

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -5,26 +5,26 @@ import "leaflet-defaulticon-compatibility";
 
 const Map = () => {
   return (
-    <MapContainer center={[9.529231241859854, 76.97208160791118]} zoom={12} scrollWheelZoom={false} style={{height: "100%", width: "100%"}}>
+    <MapContainer center={[37.48694350289257, -120.42559057997666]} zoom={12} scrollWheelZoom={false} style={{height: "100%", width: "100%"}}>
       <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
       <Marker 
-      position={[9.562792560477854, 77.00863371784253]}
+      position={[37.48694350289257, -120.42559057997666]}
       draggable={false}
       animate={true}
       >
         <Popup>
             <div>
                 <div>
-                    <strong>Location:</strong> Peermade
+                    <strong>Location:</strong> California
                 </div>
                 <div>
-                    <strong>Type:</strong> Landslide
+                    <strong>Type:</strong> Earthquake
                 </div>
                 <div>
-                    <strong>Severity:</strong> High
+                    <strong>Severity:</strong> Low
                 </div>
             </div>
         </Popup>
